25edb6e823e56c8c26519518f835c0c89af9011f
rl555 <rl555@users.noreply.github.com>
2020-07-17 16:34:16 +0200

    Improve unit test's code

--- src/test/java/hu/bme/mondokm/ltlverif/FunctionalTest.java
+++ src/test/java/hu/bme/mondokm/ltlverif/FunctionalTest.java
@@ -1,17 +1,12 @@
 package hu.bme.mondokm.ltlverif;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,14 +18,11 @@ import hu.bme.mit.theta.analysis.pred.PredPrec;
 import hu.bme.mit.theta.cfa.CFA;
 import hu.bme.mit.theta.cfa.dsl.CfaDslManager;
 import hu.bme.mit.theta.core.decl.VarDecl;
-import hu.bme.mit.theta.ltlverif.dsl.gen.LTLGrammarLexer;
-import hu.bme.mit.theta.ltlverif.dsl.gen.LTLGrammarParser;
 import hu.mondokm.ltlverif.CegarVerifier;
-import hu.mondokm.ltlverif.antlr.APGeneratorVisitor;
-import hu.mondokm.ltlverif.antlr.ToStringVisitor;
 import hu.mondokm.ltlverif.buchi.AutomatonBuilder;
 import hu.mondokm.ltlverif.buchi.BuchiAutomaton;
 import hu.mondokm.ltlverif.cfa.CfaSUT;
+import hu.mondokm.ltlverif.ltl.LTLExpr;
 
 @RunWith(Parameterized.class)
 public class FunctionalTest {
@@ -44,7 +36,7 @@ public class FunctionalTest {
     @Parameter(2)
     public boolean safety;
     
-    private static final String ERR_CONSTR = "FG (not err)";
+    private static final String ERR_CONSTR = "XG (not err)";
 
     @Parameters()
     public static Collection<Object[]> data() {
@@ -61,31 +53,9 @@ public class FunctionalTest {
         // for all paths, globally not error from some point in the future
         // AG F(
     }
-	
-    @Test
-	public void test() throws IOException, InterruptedException {
-        long startTime = System.currentTimeMillis();
-        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-
-        InputStream inputStream = new FileInputStream(getClass().getResource(filepath).getFile());
-        CFA cfa = CfaDslManager.createCfa(inputStream);
-        HashMap<String,VarDecl<?>> vars=new HashMap<>();
-        for(VarDecl<?> decl:cfa.getVars()){
-            vars.put(decl.getName(),decl);
-        }
-        HashMap<String,Integer> literalToIntMap = new HashMap<String, Integer>();
-
-        LTLGrammarLexer ltlLexer=new LTLGrammarLexer(CharStreams.fromString(LTL));
-        CommonTokenStream ltlTokenStream=new CommonTokenStream(ltlLexer);
-        LTLGrammarParser ltlParser=new LTLGrammarParser(ltlTokenStream);
-        LTLGrammarParser.ModelContext ltlModel =ltlParser.model();
-        ToStringVisitor toStringVisitor=new ToStringVisitor(new APGeneratorVisitor(vars, literalToIntMap));
-        System.out.println();
-        String ltlExpr=toStringVisitor.visitModel(ltlModel);
-        System.out.println(ltlExpr);
-        System.out.println(toStringVisitor.getAps().values());
-
-        // TODO append to PATH instead
+    
+    InputStream getAutomatonFromLTL(String ltlExpr) throws IOException, InterruptedException {
+    	// TODO append to PATH instead
         ProcessBuilder processBuilder=new ProcessBuilder("/home/user/usr/bin/ltl2tgba","!("+ltlExpr+")", "-CB");
         var processEnv = processBuilder.environment();
         var oldLibPath = processEnv.getOrDefault("LD_LIBRARY_PATH", "");
@@ -93,20 +63,47 @@ public class FunctionalTest {
         Process process=processBuilder.start();
         process.waitFor();
         
-        String errorText = new BufferedReader(new InputStreamReader(process.getErrorStream()))
-        		  .lines().collect(Collectors.joining("\n"));
+        return process.getInputStream();
+    }	
+	
+    private InputStream getAutomatonFromFile() throws IOException, InterruptedException {
+        var fis = new FileInputStream("theta/cache/" + filepath.substring(5));
+        return fis;
+    }
+    
+    private CFA loadCFA() throws IOException {
+    	InputStream inputStream = new FileInputStream(getClass().getResource(filepath).getFile());
+        return CfaDslManager.createCfa(inputStream);
+    }
+	
+    @Test
+	public void test() throws IOException, InterruptedException {
+        long startTime = System.currentTimeMillis();
+        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        CFA cfa = loadCFA();
         
-        if (!errorText.trim().isEmpty()) {
-        	
-            System.err.println(errorText);
-        	throw new IOException("LTL2TGBA failed:\n" + errorText);
+        HashMap<String,VarDecl<?>> vars=new HashMap<>();
+        for(VarDecl<?> decl:cfa.getVars()){
+            vars.put(decl.getName(),decl);
         }
+        
+        var ltlExpr = LTLExpr.from(LTL, vars);
+        /*LTLGrammarLexer ltlLexer=new LTLGrammarLexer(CharStreams.fromString(LTL));
+        CommonTokenStream ltlTokenStream=new CommonTokenStream(ltlLexer);
+        LTLGrammarParser ltlParser=new LTLGrammarParser(ltlTokenStream);
+        LTLGrammarParser.ModelContext ltlModel =ltlParser.model();
+        ToStringVisitor toStringVisitor=new ToStringVisitor(new APGeneratorVisitor(vars, literalToIntMap));
+        String ltlExpr=toStringVisitor.visitModel(ltlModel);
+        System.out.println(ltlExpr);
+        System.out.println(toStringVisitor.getAps().values());*/
+
+        var is = getAutomatonFromLTL(ltlExpr.getLTLExpr());
 
         AutomatonBuilder builder=new AutomatonBuilder();
-        builder.setAps(toStringVisitor.getAps());
-        BuchiAutomaton automaton=builder.parseAutomaton(process.getInputStream());
+        builder.setAps(ltlExpr.getAps());
+        BuchiAutomaton automaton=builder.parseAutomaton(is);
 
-        var result = CegarVerifier.verifySUT(new CfaSUT(cfa),automaton, PredPrec.of(toStringVisitor.getAps().values()));
+        var result = CegarVerifier.verifySUT(new CfaSUT(cfa),automaton, PredPrec.of(ltlExpr.getAps().values()));
         boolean resultSafety = result.isSafe();
         //System.out.println(result.asUnsafe().)
         
