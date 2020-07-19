c652c4c4832000ad3f89cb74499a91309108e28e
radl97 <radl97@users.noreply.github.com>
2020-07-19 12:53:53 +0200

    Update LTLExprVisitor.java

--- src/main/java/hu/mondokm/ltlverif/antlr/LTLExprVisitor.java
+++ src/main/java/hu/mondokm/ltlverif/antlr/LTLExprVisitor.java
@@ -4,10 +4,11 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 import java.util.HashMap;
 
-/** Returns whether an AST element is an LTL expression that has no temporal operators. (and it is maximal)
+/** Returns whether an AST element represents an LTL expression that has no temporal operators.
  *  We need to convert all these into atomic propositions that Spot can interpret.
  *  So in the AST, the F G(not err), the largest expression (not err) will be converted to atomic proposition ap0.
  *  The resulting LTL expression, which now Spot can interpret, is F G ap0.
+ *  Whether there is an LTL expression, is returned by LTLExprVisitor.
  *  The link is stored in APGeneratorVisitor's result.
  */
 public class LTLExprVisitor extends LTLGrammarBaseVisitor<Boolean> {
